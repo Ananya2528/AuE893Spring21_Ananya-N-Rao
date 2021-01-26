3efa538a2f16a6591ed833db1a7eae0924f62b85
Ananya <anagabh@clemson.edu>
2021-01-25 21:58:59 -0500

    My first git

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/annie/AuE893/src
+# Build directory: /home/annie/AuE893/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("tutorial")
---
+++ catkin_ws/build/tutorial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/annie/AuE893/src/tutorial
+# Build directory: /home/annie/AuE893/build/tutorial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
